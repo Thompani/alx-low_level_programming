dd43558bc6093ab81f2bb6b318b91096326dc699
ANIEKAN <greatestthompson@gmali.com>
2022-10-13 19:59:44 -0700

    c_programing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,16 @@
  */
 int main(void)
 {
-  printf("%lu\n", sizeof(char));
+int typeofinteger;
+long int longinteger;
+char charaacter;
+float floatingtype;
+long long int longerinteger;
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of an int: %ld byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+printf("Size of a float: %ld byte(s)\n", sizeof(float));
+printf("%lu\n", sizeof(char));
 return (0);
 }
