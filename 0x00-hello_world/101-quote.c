692043fc7794c5c6cacddf00a0d8ece61bae10fc
ANIEKAN <greatestthompson@gmali.com>
2022-10-13 20:24:11 -0700

    c_programing

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the string at the puts function
+ * Description: using the main function
+ * this program prints "programming is like building a multiligual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+  putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+  putchar("\n");
+return (0);
+}
