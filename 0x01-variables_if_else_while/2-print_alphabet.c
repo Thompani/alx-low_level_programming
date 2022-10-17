80b5650aedc8f7b7d967cfa326d8d0c8de0c7b0c
ANIEKAN <greatestthompson@gmali.com>
2022-10-17 08:29:59 -0700

    okay

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints if a number is zero, negative or positive
+ * Return: 0
+ */
+int main(void)
+{
+
+char letters;
+ 
+for (letters = 'a'; letters <= 'z'; letters++)
+{
+putchar("letters")
+}
+putchar("\n")
+return (0);
+}
