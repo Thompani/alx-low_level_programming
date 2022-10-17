14e963ada9087b050add0faf3bb5d8ce09d2eec9
ANIEKAN <greatestthompson@gmali.com>
2022-10-17 08:14:07 -0700

    okay

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
  * main - entry point
  * Description: prints if a number is zero, negative or positive
  * Return: 0
- * */
+ */
 int main(void)
 {
 
@@ -22,6 +22,10 @@ printf("%d is positive\n", n);
 else if (n < 0)
 {
 printf("%d is negative\n", n);
+}
+ else
+{
+printf("%d is zero\n", n);
 }
 return (0);
 }
