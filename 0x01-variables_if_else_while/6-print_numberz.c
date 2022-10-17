3537c1e8905203412ed8f35277d000b308026ab9
ANIEKAN <greatestthompson@gmali.com>
2022-10-17 13:13:21 -0700

    okay

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints if a number is zero, negative or positive
+ * Return: 0
+ */
+int main(void)
+{
+
+int letters;
+
+for (letters = 0; letters < 10; letters++)
+{
+  putchar( (letters % 10) + '0');
+}
+printf("\n");
+return (0);
+}
