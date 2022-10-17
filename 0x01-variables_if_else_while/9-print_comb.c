09f6256f42e3c015210e47abe728709145a45cdd
ANIEKAN <greatestthompson@gmali.com>
2022-10-17 14:21:36 -0700

    okay

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,11 @@ int main(void)
 
 int letters;
 
-for (letters = 0; letters < 10; letters++)
+for (letters = 0; letters <= 9; letters++)
 {
 putchar((letters % 10) + '0');
 putchar(',');
-putchar(' '); 
+putchar(' ');
 }
 putchar('\n');
 return (0);
