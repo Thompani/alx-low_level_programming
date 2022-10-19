9dc50caa1549a76e0a749cb52fbc25beb430ad9a
ANIEKAN <greatestthompson@gmali.com>
2022-10-18 20:09:38 -0700

    okay

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - entry point
+ * Description: A program that displays the 9 times table.
+ * Return: last digit number
+ */
+int print_last_digit(int p);
+{
+
+int n
+
+n = (p % 10);
+return (n)
+}
