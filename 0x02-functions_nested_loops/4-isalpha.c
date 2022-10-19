5a4fe0c31cab80c66ff6478fb00333723d7fdae6
ANIEKAN <greatestthompson@gmali.com>
2022-10-19 03:07:02 -0700

    okay

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - entry point
+ * Description: Write a function that prints the alphabet, in lowercase.
+ *Return: true value
+ */
+int _islower(int c)
+{
+
+if (c == 'c' || c == 'C')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
