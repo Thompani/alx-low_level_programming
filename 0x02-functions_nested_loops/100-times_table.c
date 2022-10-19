cf516aa17bff29c13621bb33b827b9d470c20007
anithomp <thompsonabove@gmail.com>
2022-10-19 19:53:50 +0100

    vjvjv

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,7 +26,7 @@ void print_times_table(int n)
 				_putchar(' ');
 				_putchar(k + '0');
 		}
-			else if (j > 10 && j < 100)
+			else if (j >= 10 && j < 100)
 			{
 				_putchar(',');
 				_putchar(' ');
