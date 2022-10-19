c3cb02133f11f3558c047772a9b1a96b8d4eb70b
ANIEKAN <greatestthompson@gmali.com>
2022-10-18 21:01:53 -0700

    okay

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - entry point
+ * Description: A program that displays the 9 times table.
+ *@c : integer value
+ * Return: last digit number
+ */
+int print_last_digit(int c)
+{
+
+_putchar('_putchar');
+return (0);
+}
