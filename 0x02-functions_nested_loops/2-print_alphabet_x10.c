54194a86b8b729aac41463a50ee30540261e7a0f
ANIEKAN <greatestthompson@gmali.com>
2022-10-18 21:24:32 -0700

    okay

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet - entry point
+ * print_alphabet_x10 - entry point
  * Description: Write a function that prints the alphabet, in lowercase.
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 char a;
 
 for (a = 'a'; a <= 'z'; a++)
 {
+for (a = 0; a <= 10; a++)
 _putchar(a);
+}
 }
 _putchar('\n');
 }
