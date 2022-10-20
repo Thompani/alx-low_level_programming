68e4ceb3fc5038f0cebb3b02ac1e70c34598c070
anithomp <thompsonabove@gmail.com>
2022-10-20 03:41:14 +0100

    okay

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,45 +7,45 @@
 */
 void print_times_table(int n)
 {
-	int p, j, k;
+	int p = 0, j, k;
 
-	if ( n <= 15 || n < 0)
+	if ( n > 15 || n < 0)
 		return;
-	for (p = 0; p <= n; p++)
+		while (p <= n)
+		 {
+	for (j = 0; j <= n; j++)
 	{
-		for (j = 0; p <= n; j++)
-		{
 			k = p * j;
-			if (j == 0)
+			if (k > 99)
 			{
-			_putchar(k + '0');
-			}
-			else if (k <= 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
 				_putchar(k + '0');
-		}
-			else if (k >= 10 && k < 100)
+				_putchar(k / 100 + '0');
+				_putchar((k / 10 % 10) + '0');
+				_putchar(k % 10 + '0');
+			}
+			else if (k > 9)
 			{
-				_putchar(',');
-				_putchar(' ');
 				_putchar(' ');
 				_putchar(k / 10 + '0');
 				_putchar(k % 10 + '0');
+			}
+			else if (y != 0)
+			{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(k + '0');
 		}
 		else
-			{
-				_putchar(',');
-				_putchar(' ');
-		_putchar((k / 100) + '0');
-		_putchar(k % 10 % 10 + '0');
-		_putchar(k % 10 + '0');
+_putchar(k + '0');
+
+				if (j !=n)
+				{
+			_putchar(',');
+			_putchar(' ');
 	}
 		}
 
 	_putchar('\n');
+	p++
 }
 }
