ffa0c7ce1de35d765a24101533287be06bb40a89
anithomp <thompsonabove@gmail.com>
2022-10-20 03:53:29 +0100

    okay

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,48 @@
 #include "main.h"
 /**
-*prints_times_table - entry point
+*print_times_table - entry point
 * @n: the starting integer
 * Description: A program that displays the 9 times table.
 * Return: void
 */
 void print_times_table(int n)
 {
-	int p = 0, j, k;
+int p = 0, j, k;
 
-	if ( n > 15 || n < 0)
-		return;
-	while (p <= n)
-	{
-			 for (j = 0; j <= n; j++)
-			 {
-			k = p * j;
-			if (k > 99)
-			{
-				_putchar(k / 100 + '0');
-				_putchar((k / 10 % 10) + '0');
-				_putchar(k % 10 + '0');
-			}
-			else if (k > 9)
-			{
-				_putchar(' ');
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
-			}
-			else if (j != 0)
-			{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(k + '0');
-		}
-		else
+if (n > 15 || n < 0)
+return;
+while (p <= n)
+{
+for (j = 0; j <= n; j++)
+{
+k = p * j;
+if (k > 99)
+{
+_putchar(k / 100 + '0');
+_putchar((k / 10 % 10) + '0');
+_putchar(k % 10 + '0');
+}
+else if (k > 9)
+{
+_putchar(' ');
+_putchar(k / 10 + '0');
+_putchar(k % 10 + '0');
+}
+else if (j != 0)
+{
+_putchar(' ');
+_putchar(' ');
 _putchar(k + '0');
-
-				if (j !=n)
-				{
-			_putchar(',');
-			_putchar(' ');
-	}
-		}
-
-	_putchar('\n');
-	p++;
+}
+else
+_putchar(k + '0');
+if (j !=n)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+p++;
 }
 }
