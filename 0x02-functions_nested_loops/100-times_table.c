06a4e7e547eada963e872c67ffce431a7669ceae
anithomp <thompsonabove@gmail.com>
2022-10-20 03:49:37 +0100

    okay

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,7 +18,6 @@ void print_times_table(int n)
 			k = p * j;
 			if (k > 99)
 			{
-				_putchar(k + '0');
 				_putchar(k / 100 + '0');
 				_putchar((k / 10 % 10) + '0');
 				_putchar(k % 10 + '0');
