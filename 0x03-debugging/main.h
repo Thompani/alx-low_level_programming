3485bacea74993510e233a078ffd38993c416a9c
anithomp <thompsonabove@gmail.com>
2022-10-21 05:22:44 +0100

    good

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef _H_MAIN
+#define _H_MAIN
+
+int positive_or_negative( int i);
+int largest_number(int, int, int);
+int print_remaining_days(int, int, int);
+int convert_day(int month, int day);
+
+#endif /* MAIN_H */
